2a8bf75af3c7a4d9a7dd13785827077982f16d3b
defmex <def>
2024-11-16 18:43:35 -0300

    correccion de nombre de proyecto

--- Backend/backendDjango/urls.py
+++ Backend/backendDjango/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backdDjango project.
+URL configuration for backendDjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
