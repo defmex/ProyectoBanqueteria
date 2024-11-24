40bc432471497a229621f100b444537e37db331b
defmex <def>
2024-11-24 20:47:22 -0300

    modificacion de link_img

--- Backend/eventos/models.py
+++ Backend/eventos/models.py
@@ -5,17 +5,20 @@ from usuario.models import Cliente
 class ComponenteBanqueteria(models.Model):
     tipo = models.CharField(max_length=100)
     costo = models.IntegerField()
+    link_img = models.CharField(max_length=255, null=True, blank=True)
     
 class Banqueteria(models.Model):
     tipo = models.CharField(max_length=100)
     costo = models.IntegerField()
     componentes = models.ManyToManyField(ComponenteBanqueteria)
+    link_img = models.CharField(max_length=255, null=True, blank=True)
 
 class Local(models.Model):
     nombre_local = models.CharField(max_length=100)
     ciudad = models.CharField(max_length=100)
     capacidad = models.IntegerField()
     precio = models.IntegerField()
+    link_img = models.CharField(max_length=255, null=True, blank=True)
 
 class Evento(models.Model):
     num_personas = models.IntegerField()
